6ece8bd7d2963fbea485cb3a408e2cc7b3d1a777
Gorm Reventlow <gorm@reventlow.com>
2022-09-17 22:24:48 +0200

    completed login test

--- src/lectio.py
+++ src/lectio.py
@@ -3,71 +3,70 @@ import requests
 import bs4
 from decouple import config
 from selenium.common.exceptions import SessionNotCreatedException, WebDriverException, NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 import datetime
 import sys
 from typing import List
+import json
 
 
 
-max_try_attempts = 100
-try_attempt = 0
+def lectio_login(school_id :int, lectio_user :str, lectio_password :str, browser):
+    max_try_attempts = 100
 
-def lectio_login(school_id):
-    now = datetime.datetime.now()
+    this_url = f"https://www.lectio.dk/lectio/{school_id}/login.aspx"
 
     #go to login page
     try:
-        browser.get(lectio_url_login)
+        browser.get(this_url)
     except:
-        error_msg = "FAG_eval_rpa crashed when trying to access lectio login page"
-        send_sms.sms_troubleshooters(error_msg)
-        sys.exit()
+        return {'msg': 'Could not load page', 'success': False}
 
     #insert username in lectio login, user field
     try_attempt = 0
     while try_attempt != max_try_attempts:
         try:
-            input_username = browser.find_element_by_id("username")
+            input_username = browser.find_element("id", "username")
             input_username.send_keys(lectio_user)
             try_attempt = max_try_attempts
         except NoSuchElementException:
             if try_attempt == max_try_attempts-1:
-                error_msg = f"{now}: FAG_eval_rpa crashed when trying to type the username in lectio login page"
-                log.log("FAG_eval_rpa crashed when trying to type the username in lectio login page")
-                send_sms.sms_troubleshooters(error_msg)
-                sys.exit()
-            try_attempt = try_attempt + 1
+                return {'msg': 'Could not insert username on login page. Reason maybe webpage is not being loaded correctly.', 'success': False}
+            try_attempt += 1
 
     #insert password in lectio login page, password field
     try_attempt = 0
     while try_attempt != max_try_attempts:
         try:
-            input_password = browser.find_element_by_id("password")
+            input_password = browser.find_element("id", "password")
             input_password.send_keys(lectio_password)
             try_attempt = max_try_attempts
         except NoSuchElementException:
             if try_attempt == max_try_attempts-1:
-                error_msg = f"{now}: FAG_eval_rpa crashed when trying to type the password in lectio login page"
-                log.log("FAG_eval_rpa crashed when trying to type the password in lectio login page")
-                send_sms.sms_troubleshooters(error_msg)
-                sys.exit()
-            try_attempt = try_attempt + 1
+                return {'msg': 'Could not insert password on login page. Reason maybe webpage is not being loaded correctly.', 'success': False}
+            try_attempt += 1
 
     try_attempt = 0
     while try_attempt != max_try_attempts:
         try:
-            button_login = browser.find_element_by_id("m_Content_submitbtn2")
+            button_login = browser.find_element("id", "m_Content_submitbtn2")
             button_login.click()
             try_attempt = max_try_attempts
         except NoSuchElementException:
             if try_attempt == max_try_attempts-1:
-                error_msg = f"{now}: FAG_eval_rpa crashed when trying to click the login button in lectio login page"
-                log.log("FAG_eval_rpa crashed when trying to click the login button in lectio login page")
-                send_sms.sms_troubleshooters(error_msg)
-                sys.exit()
+                return {'msg': 'Could not push the login button. Reason maybe webpage is not being loaded correctly.', 'success': False}
             try_attempt = try_attempt + 1
 
+    time.sleep(1)
+    browser.get(f"https://www.lectio.dk/lectio/{school_id}/forside.aspx")
+    try:
+        current_user = browser.find_element("id", "s_m_LoginOutLink").text
+        return {'msg': 'Login successful', 'success': True}
+    except NoSuchElementException as e:
+        print(e)
+        return {'msg': 'Login failed, wrong username, password and school_id combination ', 'success': False}
 
 def lectio_send_msg(browser, this_team, this_msg, disbale_send=False):
     now = datetime.datetime.now()
@@ -168,18 +167,13 @@ def lectio_send_msg(browser, this_team, this_msg, disbale_send=False):
             try_attempt = try_attempt + 1
 
 
-def lectio_test_classname(browser, test_value :str):
-    pass
-
-
-
-def get_lectio_main_page() -> bs4.BeautifulSoup:
-    page = requests.get("https://www.lectio.dk/lectio/login_list.aspx")
+def get_webpage(this_webpage) -> bs4.BeautifulSoup:
+    page = requests.get(this_webpage)
     soup = bs4.BeautifulSoup(page.content, "html.parser")
     return soup
 
-def search_webpage_for_schools(school_name :str): # -> List[str, int]:
-    page = get_lectio_main_page()
+def search_webpage_for_schools(school_name=""): # -> List[str, int]:
+    page = get_webpage("https://www.lectio.dk/lectio/login_list.aspx")
 
     this_list :list = []
     this_dict :dict = {}
@@ -187,37 +181,53 @@ def search_webpage_for_schools(school_name :str): # -> List[str, int]:
 
     raw_schools = page.find_all('a', href=True)
     for item in raw_schools:
-        this_list.append(item)
-
-    print(type(this_list))
-    for item in this_list:
         if "default.aspx" in str(item):
-            this_id = git_digits_from_string(str(item))
-            this_school = str(item).split(">")[1:]
-            this_school = str(this_school)[:-9]
-            this_school = this_school[2:]
-            this_dict.update({'id' : str(this_id), 'school_name' : this_school})
-    print(this_dict)
+            if school_name in str(item) or school_name == "":
+                this_id = get_digits_from_string(str(item))
+                this_school = str(item).split(">")[1:]
+                this_school = str(this_school)[:-9]
+                this_school = this_school[2:]
+                this_list.append([('id' , str(this_id)), ('school_name' , this_school)])
 
+    this_dict.update(this_list)
+    this_dict = clean_for_json(this_dict)
 
-def git_digits_from_string(string :str) -> str:
+    json_object = json.dumps(this_dict, indent=4)
+    return this_dict
+
+def clean_for_json(this_dict :str) -> str:
+    this_dict = str(this_dict).replace("(", '').replace(")", '')
+    this_dict = "{'schools': [" + this_dict + "]}"
+    this_dict = this_dict.replace("'id'", "{'id")
+    this_dict = this_dict.replace("'school_name',", "'school_name' :")
+    this_dict = this_dict.replace(",", "}, ")
+    this_dict = this_dict.replace(",", "}, ")
+    this_dict = this_dict.replace("'id}},", "{'id' :")
+    this_dict = this_dict.replace(": 'school_name'", ", 'school_name'")
+    this_dict = this_dict.replace("{{", "{")
+    this_dict = this_dict.replace("}}", "}")
+    this_dict = this_dict.replace("{{", "{")
+    this_dict = this_dict.replace("}}", "}")
+    #this_dict = this_dict.replace("'", '"')
+    return  this_dict
+
+def get_digits_from_string(string :str) -> str:
     digits = ""
     for c in string:
         if c.isdigit():
             digits = digits + c
     return digits
 
-
-
+def get_webdriver() -> webdriver:
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    return browser
 
 def main():
-    #lectio_user = config('LECTIO_RPA_USER')
-    #lectio_password = config('LECTIO_RPA_PASSWORD')
-    #lectio_test_class = config('LECTIO_RPA_TEST_CLASS')
-    #lectio_url_send_msg = config('LECTIO_SEND_MSG_URL')
-    #lectio_url_login = config('LECTIO_LOGIN_URL')
-    search_webpage_for_schools("test")
-    return ["test", 1]
+    lectio_user = config('LECTIO_RPA_USER')
+    lectio_password = config('LECTIO_RPA_PASSWORD')
+    print(lectio_login(235, lectio_user, lectio_password, get_webdriver()))
+
+
 
 if __name__ == "__main__":
     main()
--- src/lectio_fastapi.py
+++ src/lectio_fastapi.py
@@ -1,28 +1,28 @@
-from typing import Union
 from fastapi import FastAPI
-
-
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+from . import lectio
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"lectio_school_id": "int", }
+def read_root():
+    return {""}
 
-
-@app.get("/message_send/{lectio_school_id, lectio_user, lectio_password, lectio_subject, lectio_msg, lectio_can_answer, lectio_test}")
-def send_msg(lectio_school_id: int, lectio_user: str, lectio_password: str, lectio_subject: str, lectio_msg: str, lectio_can_answer: bool, lectio_test: bool):
-    return {"lectio_school_id": lectio_school_id}
+@app.get("/school_ids/{lectio_school_name}")
+def get_school_id(lectio_school_name: str):
+    json_object = lectio.search_webpage_for_schools(lectio_school_name)
+    #json_object = jsonable_encoder(json_object)
+    return JSONResponse(content=json_object)
 
 @app.get("/message_send/{lectio_school_id, lectio_user, lectio_password}")
 def test_login(lectio_school_id: int, lectio_user: str, lectio_password: str):
-    login_successful = False
-    return {"login_successful": login_successful}
+    lectio_login_result = lectio.lectio_login(lectio_school_id, lectio_user, lectio_password, lectio.get_webdriver())
+    return lectio_login_result
 
-@app.get("/school_ids/{lectio_school_id, lectio_school_name}")
-def test_login(lectio_school_id: int, lectio_school_name: str):
-    school_list = {}
-    return school_list
+@app.post("/message_send/{lectio_school_id, lectio_user, lectio_password, lectio_subject, lectio_msg, lectio_can_answer}")
+def send_msg(lectio_school_id: int, lectio_user: str, lectio_password: str, lectio_subject: str, lectio_msg: str, lectio_can_answer: bool):
+    return {"lectio_school_id": lectio_school_id}
 
 def main():
     pass
