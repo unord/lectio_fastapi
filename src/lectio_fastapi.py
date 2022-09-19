a3de4c420875f434004cde0b9d613c4e9ae570af
Gorm Reventlow <gorm@reventlow.com>
2022-09-19 06:57:58 +0200

    clean up code

--- src/lectio_fastapi.py
+++ src/lectio_fastapi.py
@@ -4,9 +4,16 @@ from . import lectio
 app = FastAPI()
 
 
+class SendMsg(BaseModel):
+    lectio_school_id: int
+    lectio_user: str
+    lectio_password: str
+    send_to: str
+    subject: str
+    msg: str
+    msg_can_be_replied: bool
 
-
-class GetSchoolId(BaseModel):
+class SchoolName(BaseModel):
     lectio_school_name: str
 
 
@@ -19,8 +26,8 @@ def read_root():
 def get_school_id():
     return {'msg': 'get_school_id function gets a school id that contains the string', 'success': True}
 
-@app.post("/school_ids/{lectio_school_name}")
-def get_school_id(lectio_school_name: GetSchoolId):
+@app.post("/school_ids/")
+def get_school_id(lectio_school_name: SchoolName):
     lectio_school_id_results = lectio.lectio_search_webpage_for_schools(lectio_school_name)
     return lectio_school_name
 
@@ -35,15 +42,15 @@ def test_login(lectio_school_id: int, lectio_user: str, lectio_password: str):
 def send_msg():
     return {'msg': 'message_send function for lectio', 'success': True}
 
-#@app.post("/message_send/{lectio_school_id, lectio_user, lectio_password, send_to, subject, msg, msg_can_be_replied}")
-#def send_msg(send_msg: SendMsg):
-#    browser = lectio.get_webdriver()
-#    lectio_login_result = lectio.lectio_login(send_msg.lectio_school_id, send_msg.lectio_user, send_msg.lectio_password, browser)
-#    if lectio_login_result['success']:
-#        lectio_send_msg_result = lectio.lectio_send_msg(send_msg.send_to, send_msg.subject, send_msg.msg, send_msg.msg_can_be_replied, send_msg.lectio_school_id, browser)
-#        return lectio_send_msg_result
-#    else:
-#        return {'msg': 'Login failed, wrong username, password and school_id combination ', 'success': False}
+@app.post("/message_send/")
+def send_msg(send_msg: SendMsg):
+    browser = lectio.get_webdriver()
+    lectio_login_result = lectio.lectio_login(send_msg.lectio_school_id, send_msg.lectio_user, send_msg.lectio_password, browser)
+    if lectio_login_result['success']:
+        lectio_send_msg_result = lectio.lectio_send_msg(send_msg.send_to, send_msg.subject, send_msg.msg, send_msg.msg_can_be_replied, send_msg.lectio_school_id, browser)
+        return lectio_send_msg_result
+    else:
+        return {'msg': 'Login failed, wrong username, password and school_id combination ', 'success': False}
 
 def main():
     pass
