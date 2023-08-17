7f9a03f24c62ed448e2bd9bbe0b14cd57624d686
Gorm Reventlow <gorm@reventlow.com>
2023-08-17 16:48:07 +0200

    Changing BIN to SHIM

--- src/lectio.py
+++ src/lectio.py
@@ -46,7 +46,7 @@ def get_digits_from_string(string: str) -> str:
 
 def get_webdriver() -> webdriver:
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
